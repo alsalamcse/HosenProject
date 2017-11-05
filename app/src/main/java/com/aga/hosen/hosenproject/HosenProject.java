02846e919ca491c4d6455526484e3e6a4edafed5
user <hosenaga@gmail.com>
2017-11-05 09:24:39 +0200

    Initial commit

---
+++ app/src/main/java/com/aga/hosen/hosenproject/HosenProject.java
@@ -0,0 +1,13 @@
+package com.aga.hosen.hosenproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HosenProject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hosen_project);
+    }
+}
