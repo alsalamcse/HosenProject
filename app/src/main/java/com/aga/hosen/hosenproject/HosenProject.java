94a3c983b7970b99068d12856efe2009ff4ef7ce
user <hosenaga@gmail.com>
2018-03-05 09:30:48 +0200

    ghf

--- app/src/main/java/com/aga/hosen/hosenproject/HosenProject.java
+++ app/src/main/java/com/aga/hosen/hosenproject/HosenProject.java
@@ -2,12 +2,44 @@ package com.aga.hosen.hosenproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HosenProject extends AppCompatActivity {
+
+    private TextView tvclient;
+    private EditText etphone;
+    private EditText etemail;
+    private EditText etpassw;
+    private Button btnsignup;
+    private TextView tvmanager;
+    private EditText etemail2;
+    private EditText etpassw2;
+    private Button etsignin;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hosen_project);
+        tvclient = (TextView) findViewById(R.id.tvclient);
+        etphone = (EditText) findViewById(R.id.etphone);
+        etemail = (EditText) findViewById(R.id.etemail);
+        etpassw = (EditText) findViewById(R.id.etpassw);
+        btnsignup = (Button) findViewById(R.id.btnsignup);
+        tvmanager = (TextView) findViewById(R.id.tvmanager);
+        etemail2 = (EditText) findViewById(R.id.etemail2);
+        etpassw2 = (EditText) findViewById(R.id.etpassw2);
+        etsignin = (Button) findViewById(R.id.etsignin);
+
+
+
+
+
+
+
     }
 }
